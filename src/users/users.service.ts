bf534c9fc103964e7ac54a7eb75a9e47950d2e78
decopro <dc732007@gmail.com>
2021-08-28 02:15:42 +0300

    crud

---
+++ src/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+
+    constructor(@InjectRepository(User) private usersRepository: Repository<User>) { }
+
+    async createUser(user: User) {
+        // await this.usersRepository.create(user);
+        await this.usersRepository.create(user);
+    }
+
+    async getUsers(user: User): Promise<User[]> {
+        return await this.usersRepository.find();
+    }
+
+    async getUser(_id: number): Promise<User[]> {
+        return await this.usersRepository.find({
+            select: ["firstname", "secondname" ],
+            where: [{ "id": _id }]
+        });
+    }
+
+    async updateUser(user: User) {
+        this.usersRepository.save(user)
+    }
+
+    async deleteUser(user: User) {
+        this.usersRepository.delete(user);
+    }
+}
